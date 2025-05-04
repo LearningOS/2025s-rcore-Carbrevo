b2d40b2d7332fae24fbac211d58fd2994b48bef0
Pablo <carbrevo@hotmail.com>
2025-05-04 11:56:54 +0800

    0504

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -35,7 +35,7 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
             sec: us / 1_000_000,
             usec: us % 1_000_000,
         };
-        assert(!(ts.sec <= 0 && ts.usec <= 0));
+        assert!(!((*ts).sec <= 0 && (*ts).usec <= 0));
     }
     0
 }
